b8f7aa1ea8f1cae36e05f0b1ac95433b55047e7f
Jesyka Palmer <jmpalmer@us.ibm.com>
2016-06-09 11:19:15 -0700

    next two months

--- 2016-may/domonthreport.R
+++ 2016-may/domonthreport.R
@@ -9,8 +9,8 @@ header <- function(title){
 }
 
 #main report title and date
-header("2016 Monthly Wholesale Summary ran on April 7, 2016")
+header("2016 Monthly Wholesale Summary ran on June 9, 2016")
 
 #Monthly summary
-header("2016 Wholesale Totals | Mar 2015 compared to Mar 2016")
-report("fixed_orders.csv", month1= "03", month2= "03", year1="16", year2="15")
+header("2016 Wholesale Totals | May 2016 compared to April 2016")
+report("fixed_orders.csv", month1= "05", month2= "04", year1="16", year2="16")
